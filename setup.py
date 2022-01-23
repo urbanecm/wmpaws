4675db8daa382013b51a8477b72a2d106efe7af8
Martin Urbanec <martin@urbanec.cz>
2022-01-23 19:18:21 +0100

    Bump version to 1.0
    Suggested to install the patch for all
    the WM PAWS users => version should be >= 1.0.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='wmpaws',
-    version='0.9',
+    version='1.0',
     license='GNU',
     author='Martin Urbanec',
     author_email='martin@urbanec.cz',
